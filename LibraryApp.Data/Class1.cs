fd08a18ba95c2bfd96bf76de45c2c830799c63bb
Carlos Alberto Escobedo Moreno <cescobedo@centrointeligente.com>
2025-03-07 17:54:53 -0600

    temp commit

--- LibraryApp.Data/Class1.cs
+++ LibraryApp.Data/Class1.cs
@@ -1,7 +1,22 @@
-﻿namespace LibraryApp.Data
+﻿using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace LibraryApp.Data
 {
     public class Connection
     {
+        private string connectionString;
 
+        public Connection()
+        {
+            connectionString = "Server=localhost;Database=libary_db;User ID=root;Password=root";
+        }
+        
+        public DbConnection CreateConnection()
+        {
+            MySqlConnection dbConnection = new MySqlConnection(connectionString);
+            dbConnection.Open();
+            return dbConnection;
+        }
     }
 }
